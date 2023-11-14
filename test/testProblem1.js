36299da6bfe06d9492b110b7627bbda08a915839
Lokesh Bottu <82814830+lokesh-bottu@users.noreply.github.com>
2023-11-14 16:29:38 +0530

    Add files via upload

---
+++ test/testProblem1.js
@@ -0,0 +1,7 @@
+const Problem1 = require("../async_js_exercises/Problem-1/CallBack");
+const Problem2 = require("../async_js_exercises/Problem-1/Promis");
+const Problem3 = require("../async_js_exercises/Problem-1/async-await");
+
+Problem1();
+Problem2();
+Problem3();
---
+++ test/testProblem2.js
@@ -0,0 +1,3 @@
+const main = require("../async_js_exercises/Problem-2/Problem2");
+
+main();
